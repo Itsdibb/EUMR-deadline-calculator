3e0f0db736822a7905044963e2dba9decc1050b4
Itsdibb <103038452+Itsdibb@users.noreply.github.com>
2023-09-08 16:01:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,11 @@ from PIL import Image
 from deta import Deta
 from io import BytesIO
 
+# Access the Data Key from Streamlit secrets
+data_key = st.secrets["data_key"]
+
 # Replace 'your_project_key' with your actual Deta project key.
-deta = Deta('a05n6m237pg_eAeKLR4a87uSMGwSqUnK4J17ViG8F7ES')
+deta = Deta(data_key)
 
 # Name your drive
 drive = deta.Drive("Data")
